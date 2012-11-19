478eefc388bd5ba6d6e189a0ad8eb3f8a84e9e16
Jan Hartigan <janhartigan13@gmail.com>
2012-11-18 23:56:02 -0800

    changing titles to title config instead of ucwords($key)

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -7,7 +7,7 @@
 		@foreach (Config::get('administrator::administrator.models') as $key => $model)
 			<?php $key = is_numeric($key) ? $model : $key; ?>
 			<li class="@if ($modelName == $key) current @endif">
-				{{ HTML::link(URL::to_route('admin_index', array($key)), ucfirst($key)) }}
+				{{ HTML::link(URL::to_route('admin_index', array($key)), $model['title']) }}
 			</li>
 		@endforeach
 	</ul>
