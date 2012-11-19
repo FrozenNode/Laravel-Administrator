8ed0831f0fdfd024c6e4cde8d25709d3926a9cc5
Jan Hartigan <janhartigan13@gmail.com>
2012-11-18 23:54:26 -0800

    changing dashboard to title instead of ucwords($key)

--- views/dashboard.blade.php
+++ views/dashboard.blade.php
@@ -1,11 +1,11 @@
 <div id="dashboard">
 	<h2>Dashboard</h2>
-	
+
 	<ul class="models">
 		@foreach (Config::get('administrator::administrator.models') as $key => $model)
 			<?php $key = is_numeric($key) ? $model : $key; ?>
 			<li>
-				{{ HTML::link(URL::to_route('admin_index', array($key)), ucfirst($key)) }}
+				{{ HTML::link(URL::to_route('admin_index', array($key)), $model['title']) }}
 			</li>
 		@endforeach
 	</ul>
