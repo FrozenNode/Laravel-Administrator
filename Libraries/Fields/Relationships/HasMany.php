1dd0b14c9b2d913dc71484eaf36b8ce4473ec6ac
Jan Hartigan <janhartigan13@gmail.com>
2012-11-25 15:26:25 -0800

    adding Libraries back with capital L (closes #6)

---
+++ Libraries/Fields/Relationships/HasMany.php
@@ -0,0 +1,69 @@
+<?php
+namespace Admin\Libraries\Fields\Relationships;
+
+use Admin\Libraries\Column;
+
+class HasMany extends Relationship {
+
+	/**
+	 * This determines if there are potentially multiple related values (i.e. whether to use an array of items or just a single value)
+	 *
+	 * @var bool
+	 */
+	public $multipleValues = true;
+
+	/**
+	 * Constructor function
+	 *
+	 * @param string|int	$field
+	 * @param array|string	$info
+	 * @param Eloquent 		$model
+	 */
+	public function __construct($field, $info, $model)
+	{
+		parent::__construct($field, $info, $model);
+
+		$relationship = $model->{$field}();
+
+		$this->table = $relationship->table->from;
+		$this->column = $relationship->table->wheres[0]['column'];
+	}
+
+	/**
+	 * Filters a query object with this item's data given a model
+	 *
+	 * @param Query		$query
+	 * @param Eloquent	$model
+	 *
+	 * @return void
+	 */
+	public function filterQuery(&$query, $model)
+	{
+		//run the parent method
+		parent::filterQuery($query, $model);
+
+		//if there is no value, return
+		if (!$this->value)
+		{
+			return;
+		}
+
+		//if the table hasn't been joined yet, join it
+		if (!Column::isJoined($query, $this->table))
+		{
+			$query->join($this->table, $model->table().'.'.$model::$key, '=', $this->table.'.'.$this->column);
+		}
+
+
+		$query->where_in($this->table.'.id', (is_array($this->value) ? $this->value : array($this->value)));
+	}
+
+	/**
+	 * For the moment this is an empty function until I can figure out a way to display HasOne and HasMany relationships on this model
+	 *
+	 * @param Eloquent	$model
+	 *
+	 * @return array
+	 */
+	public function fillModel(&$model, $input) {}
+}
