cec4424c2b3897a36cbaf013fbda1d673c0905c3
Przemysław Kamiński <przemyslaw.kaminski@gmail.com>
2013-01-26 16:08:29 +0100

    Change to the Image field It now supports two ways of specifying upload path either by: 'location' => 'path/relative/to/public/' or by: 'location_upload_path' and 'location_upload_url' (first one specifies absolute path for upload, and second the return url to which attach filename).
    Also, 'location' supports 3 types of locations:
    path('public').'some/path'
    'public/some/path' and
    'some/path'

--- Libraries/Fields/Image.php
+++ Libraries/Fields/Image.php
@@ -13,11 +13,28 @@ class Image extends Field {
 	public $naming = 'random';
 
 	/**
-	 * The directory location used to store the original
+	 * The relative location used to store the original
+	 * within public directory
 	 *
 	 * @var string
 	 */
 	public $location;
+
+	/**
+	 * The url for getting preview
+	 * (when using $location_path).
+	 *
+	 * @var string
+	 */
+	public $location_upload_url;
+
+	/**
+	 * The directory location used to store the original
+	 * (when not using $location)
+	 *
+	 * @var string
+	 */
+	public $location_upload_path;
 
 	/**
 	 * The upload url for this field
@@ -66,15 +83,29 @@ class Image extends Field {
 		$this->sizes = array_get($info, 'sizes', $this->sizes);
 		$this->naming = array_get($info, 'naming', $this->naming);
 		$this->location = array_get($info, 'location');
+		$this->location_upload_path = array_get($info, 'location_upload_path');
+		$this->location_upload_url = array_get($info, 'location_upload_url');
 		$this->sizeLimit = (int) array_get($info, 'size_limit', $this->sizeLimit);
 		$this->uploadUrl = \URL::to_route('admin_image_upload', array($config->name, $this->field));
 
-		$replace = 'public/';
-
-		if (strpos($this->location, $replace) === 0)
+		//Deciding what the displayUrl is
+		if($this->location_upload_url && $this->location_upload_path)
 		{
-			$this->displayUrl = \URL::to('/' . substr_replace($this->location, '', 0, strlen($replace)));
+			$this->displayUrl = $this->location_upload_url;
+			
+		} else {
+			// Bit of legacy code - user can have either:
+			// path('public').'uploads/somepath/' or
+			// 'public/uploads/somepath
+			$public_path = path('public');
+			$pattern = "#^(public/|$public_path)(.*)#";
+			if(preg_match($pattern, $this->location)) {
+				$this->displayUrl = \URL::to(preg_replace($pattern, '$2', $this->location));
+			} else {
+				$this->displayUrl = \URL::to($this->location);
+			}
 		}
+
 
 		//make sure the naming is one of the two accepted values
 		$this->naming = in_array($this->naming, array('keep', 'random')) ? $this->naming : 'random';
@@ -87,8 +118,16 @@ class Image extends Field {
 	 */
 	public function doUpload()
 	{
+		$location = '';
+		// Since there are two methods of specyfing location, deciding where to upload file
+		if($this->location_upload_url && $this->location_upload_path) {
+			$location = $this->location_upload_path;
+		} else {
+			$location = path('public') . $this->location;
+		}
+
 		//use the multup library to perform the upload
-		$result = \Admin\Libraries\Includes\Multup::open('file', 'image|max:' . $this->sizeLimit * 1000 . '|mimes:jpg,gif,png', $this->location, true)
+		$result = \Admin\Libraries\Includes\Multup::open('file', 'image|max:' . $this->sizeLimit * 1000 . '|mimes:jpg,gif,png', $location, true)
 			->sizes($this->sizes)
 			->upload();
 
@@ -105,6 +144,8 @@ class Image extends Field {
 		$arr = parent::toArray();
 
 		$arr['location'] = $this->location;
+		$arr['location_upload_path'] = $this->location_upload_path;
+		$arr['location_upload_url'] = $this->location_upload_url;
 		$arr['size_limit'] = $this->sizeLimit;
 		$arr['upload_url'] = $this->uploadUrl;
 		$arr['display_url'] = $this->displayUrl;
