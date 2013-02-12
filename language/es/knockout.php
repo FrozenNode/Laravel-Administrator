96f3a5908fa8e5a2dae66c6214c11d4a09fa9824
Martxel Lasa <martxel@gmail.com>
2013-02-12 17:48:12 +0100

    Added spanish translations.

---
+++ language/es/knockout.php
@@ -0,0 +1,22 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines for knockout
+	|--------------------------------------------------------------------------
+	|
+	| es - Spanish
+	|
+	*/
+
+	'delete_active_item' => '¿Está seguro de que desea eliminar este ítem? Esta operación es irreversible.',
+	'saving' => 'Guardando...',
+	'saved' => 'Ítem guardado.',
+	'deleting' => 'Eliminando...',
+	'deleted' => 'Ítem eliminado.',
+	'character_left' => ' carácter restante',
+	'characters_left' => ' caracteres restantes',
+
+);
