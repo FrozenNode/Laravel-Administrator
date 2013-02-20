d21591b06745706313ff07bae8e959c528a31e1d
Dawid Mac-Mini <dawid@claromentis.com>
2013-02-20 23:45:37 +0000

    Added Polish translations

---
+++ language/pl/administrator.php
@@ -0,0 +1,50 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| pl - Polish
+	|
+	*/
+
+	'dashboard'      => 'Panel',
+	'edit'           => 'Edycja',
+	'filters'        => 'Filtry',
+	'loading'        => 'Ładuje...',
+	'createnew'      => 'Stwórz nowy',
+	'new'            => 'Nowy',
+	'viewitem'       => 'wyświetl element',
+	'id'             => 'ID',
+	'uploadimage'    => 'Dodaj zdjęcie',
+	'imageuploading' => 'Ładuje zdjęcie',
+	'none'           => 'Żaden',
+	'all'            => 'Wszystkie',
+	'itemsperpage'   => 'pozycji na stronie',
+	'noresults'      => 'Brak wynikow',
+	'backtosite'     => 'Powrót do strony',
+
+	'previous' => 'poprzedni',
+	'next'     => 'następny',
+
+	'close'  => 'Zamknij',
+	'delete' => 'Usuń',
+	'save'   => 'Zapisz',
+	'create' => 'Stwórz',
+	'cancel' => 'Anuluj',
+
+	'active'  => 'Chwileczkę...',
+	'success' => 'Sukces!',
+	'error'   => 'Wystąpił błąd podczas wykonywania tej czynności',
+
+	'valid_columns'     => "Musisz podać poprawna tablicę 'columns' dla każdego zdefiniowanego modelu",
+	'valid_title'       => "Musisz podać poprawny tytuł i pojedynczą nazwę dla każdego zdefiniowanego modelu",
+	'valid_model'       => "Dla każdego zdefiniowanego modelu musisz podać jego opcje",
+	'valid_edit'        => "Musisz podać poprawną tablicę 'edit_fields' dla każdego zdefiniowanego modelu",
+	'valid_menu'        => "Musisz podać poprawną opcję 'Menu' w pliku konfiguracyjnym administrator.php",
+	'valid_config_path' => "Musisz podać poprawny 'model_config_path' w pliku konfiguracyjnym administrator.php. Katalog musi istnieć i mieć prawo do odczytu.",
+	'not_eloquent'      => " nie jest Eloquent model",
+);
