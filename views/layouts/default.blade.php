fdc69c57c54d3a104eef342efffcc167392509f1
Jan Hartigan <janhartigan13@gmail.com>
2013-04-22 13:09:40 -0700

    Removed some ancient boilerplate

--- views/layouts/default.blade.php
+++ views/layouts/default.blade.php
@@ -3,8 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<title>{{ Config::get('administrator::administrator.title') }}</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
 
 	{{ Asset::container('container')->styles() }}
 </head>
