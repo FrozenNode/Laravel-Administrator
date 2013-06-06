9cfa598ad3f6b1e61ca99c224929e0381a51dd98
Jan Hartigan <janhartigan13@gmail.com>
2013-06-06 00:46:32 -0700

    Updating default config dirs (Closes #171)

--- src/config/administrator.php
+++ src/config/administrator.php
@@ -21,14 +21,14 @@ return array(
 	 *
 	 * @type string
 	 */
-	'model_config_path' => app('path') . 'config/administrator',
+	'model_config_path' => app('path') . '/config/administrator',
 
 	/**
 	 * The path to your settings config directory
 	 *
 	 * @type string
 	 */
-	'settings_config_path' => app('path') . 'config/administrator/settings',
+	'settings_config_path' => app('path') . '/config/administrator/settings',
 
 	/**
 	 * The menu structure of the site. For models, you should either supply the name of a model config file or an array of names of model config
