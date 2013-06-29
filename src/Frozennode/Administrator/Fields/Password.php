8640dfeb80eebcc024a4408148910903137db0d0
Jan Hartigan <janhartigan13@gmail.com>
2013-06-29 13:36:09 -0700

    removing unnecessary constructor override

--- src/Frozennode/Administrator/Fields/Password.php
+++ src/Frozennode/Administrator/Fields/Password.php
@@ -9,18 +9,4 @@ class Password extends Text {
 	 * @var bool
 	 */
 	public $setter = true;
-
-	/**
-	 * Constructor function
-	 *
-	 * @param string|int	$field
-	 * @param array|string	$info
-	 * @param ModelConfig 	$config
-	 */
-	public function __construct($field, $info, $config)
-	{
-		parent::__construct($field, $info, $config);
-
-		$this->setter = array_get($info, 'setter', $this->setter);
-	}
 }
