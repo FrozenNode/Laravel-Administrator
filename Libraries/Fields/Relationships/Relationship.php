05293996918e151db6e050c538b002fe76c692ba
Pavel Puchkin <neoascetic@gmail.com>
2013-07-03 13:55:13 +1100

    Now it is possible to specify a getter in 'field_name' on relationship

--- Libraries/Fields/Relationships/Relationship.php
+++ Libraries/Fields/Relationships/Relationship.php
@@ -129,7 +129,7 @@ abstract class Relationship extends Field {
 
 		if (array_get($info, 'load_relationships', false))
 		{
-			$options = $relationship->model->order_by($this->nameField)->get();
+			$options = $relationship->model->get();
 		}
 		//otherwise if there are relationship items, we need them in the initial options list
 		else if ($relationshipItems = $relationship->get())
