4dc2e47f05fe0c062571c230955eec6bfd2481cb
Jan Hartigan <janhartigan13@gmail.com>
2013-07-11 19:02:33 -0700

    no php5.4 brackets in php5.3 :(

--- tests/Config/FactoryTest.php
+++ tests/Config/FactoryTest.php
@@ -59,17 +59,17 @@ class FactoryTest extends \PHPUnit_Framework_TestCase {
 	public function testMakeReturnsModel()
 	{
 		$configMock = m::mock('Frozennode\Administrator\Config\Model\Config');
-		$factory = m::mock('Frozennode\Administrator\Config\Factory[parseType,searchMenu,getItemConfigObject]', [$this->validator, []]);
+		$factory = m::mock('Frozennode\Administrator\Config\Factory[parseType,searchMenu,getItemConfigObject]', array($this->validator, array()));
 		$factory->shouldReceive('parseType')->once()
-				->shouldReceive('searchMenu')->once()->andReturn(['test'])
-				->shouldReceive('getItemConfigObject')->once()->with(['test'])->andReturn($configMock);
+				->shouldReceive('searchMenu')->once()->andReturn(array('test'))
+				->shouldReceive('getItemConfigObject')->once()->with(array('test'))->andReturn($configMock);
 		$this->assertEquals($factory->make('some_model'), $configMock);
 	}
 
 	public function testMakeReturnsFalse()
 	{
 		$configMock = m::mock('Frozennode\Administrator\Config\Model\Config');
-		$factory = m::mock('Frozennode\Administrator\Config\Factory[parseType,searchMenu]', [$this->validator, []]);
+		$factory = m::mock('Frozennode\Administrator\Config\Factory[parseType,searchMenu]', array($this->validator, array()));
 		$factory->shouldReceive('parseType')->once()
 				->shouldReceive('searchMenu')->once()->andReturn(false);
 		$this->assertEquals($factory->make('some_model'), false);
@@ -77,14 +77,14 @@ class FactoryTest extends \PHPUnit_Framework_TestCase {
 
 	public function testParseTypeModel()
 	{
-		$factory = new Factory($this->validator, []);
+		$factory = new Factory($this->validator, array());
 		$factory->parseType('some_model');
 		$this->assertEquals($factory->getType(), 'model');
 	}
 
 	public function testParseTypeSettings()
 	{
-		$factory = new Factory($this->validator, []);
+		$factory = new Factory($this->validator, array());
 		$factory->parseType('settings.something');
 		$this->assertEquals($factory->getType(), 'settings');
 	}
@@ -92,34 +92,34 @@ class FactoryTest extends \PHPUnit_Framework_TestCase {
 	public function testModelInMenu()
 	{
 		$name = 'some_model';
-		$config = ['menu' => [$name]];
-		$factory = m::mock('Frozennode\Administrator\Config\Factory[fetchConfigFile]', [$this->validator, $config]);
-		$factory->shouldReceive('fetchConfigFile')->once()->andReturn([]);
-		$this->assertEquals($factory->searchMenu($name), []);
+		$config = array('menu' => array($name));
+		$factory = m::mock('Frozennode\Administrator\Config\Factory[fetchConfigFile]', array($this->validator, $config));
+		$factory->shouldReceive('fetchConfigFile')->once()->andReturn(array());
+		$this->assertEquals($factory->searchMenu($name), array());
 	}
 
 	public function testModelInNestedMenu()
 	{
 		$name = 'some_model';
-		$config = ['menu' => ['foo' => [$name]]];
-		$factory = m::mock('Frozennode\Administrator\Config\Factory[fetchConfigFile]', [$this->validator, $config]);
-		$factory->shouldReceive('fetchConfigFile')->once()->andReturn([]);
-		$this->assertEquals($factory->searchMenu($name), []);
+		$config = array('menu' => array('foo' => array($name)));
+		$factory = m::mock('Frozennode\Administrator\Config\Factory[fetchConfigFile]', array($this->validator, $config));
+		$factory->shouldReceive('fetchConfigFile')->once()->andReturn(array());
+		$this->assertEquals($factory->searchMenu($name), array());
 	}
 
 	public function testModelInDeepNestedMenu()
 	{
 		$name = 'some_model';
-		$config = ['menu' => ['foo' => ['bar' => [$name]]]];
-		$factory = m::mock('Frozennode\Administrator\Config\Factory[fetchConfigFile]', [$this->validator, $config]);
-		$factory->shouldReceive('fetchConfigFile')->once()->andReturn([]);
-		$this->assertEquals($factory->searchMenu($name), []);
+		$config = array('menu' => array('foo' => array('bar' => array($name))));
+		$factory = m::mock('Frozennode\Administrator\Config\Factory[fetchConfigFile]', array($this->validator, $config));
+		$factory->shouldReceive('fetchConfigFile')->once()->andReturn(array());
+		$this->assertEquals($factory->searchMenu($name), array());
 	}
 
 	public function testModelNotInMenu()
 	{
 		$name = 'some_model';
-		$factory = new Factory($this->validator, ['menu' => []]);
+		$factory = new Factory($this->validator, array('menu' => array()));
 		$this->assertEquals($factory->searchMenu($name), false);
 	}
 
@@ -128,17 +128,17 @@ class FactoryTest extends \PHPUnit_Framework_TestCase {
 		$name = 'some_model';
 		$filename = __DIR__ . '/' . $name . '.php';
 		file_put_contents($filename, "<?php return array();");
-		$factory = m::mock('Frozennode\Administrator\Config\Factory[getPath,getPrefix]', [$this->validator, []]);
+		$factory = m::mock('Frozennode\Administrator\Config\Factory[getPath,getPrefix]', array($this->validator, array()));
 		$factory->shouldReceive('getPath')->once()->andReturn(__DIR__ . '/')
 				->shouldReceive('getPrefix')->once()->andReturn('');
-		$this->assertEquals($factory->fetchConfigFile($name), ['name' => $name]);
+		$this->assertEquals($factory->fetchConfigFile($name), array('name' => $name));
 		unlink($filename);
 	}
 
 	public function testFetchConfigFails()
 	{
 		$name = 'some_model';
-		$factory = m::mock('Frozennode\Administrator\Config\Factory[getPath,getPrefix]', [$this->validator, []]);
+		$factory = m::mock('Frozennode\Administrator\Config\Factory[getPath,getPrefix]', array($this->validator, array()));
 		$factory->shouldReceive('getPath')->once()->andReturn(__DIR__ . '/')
 				->shouldReceive('getPrefix')->once()->andReturn('');
 		$this->assertEquals($factory->fetchConfigFile($name), false);
