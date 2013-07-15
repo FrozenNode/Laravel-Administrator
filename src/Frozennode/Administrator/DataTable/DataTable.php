3b23f4cf73739891cd1e2dc4092fee62fe99d2c5
Jan Hartigan <janhartigan13@gmail.com>
2013-07-15 00:04:07 -0700

    refactoring datatable

--- src/Frozennode/Administrator/DataTable/DataTable.php
+++ src/Frozennode/Administrator/DataTable/DataTable.php
@@ -79,21 +79,23 @@ class DataTable {
 
 		//update the sort options
 		$this->setSort($sort);
-		$sort = $this->sort;
+		$sort = $this->getSort();
 
 		//get things going by grouping the set
-		$query = $model::groupBy($model->getTable().'.'.$model->getKeyName());
-		$db_query = $query->getQuery();
-		$count_query = $db->table($model->getTable())->groupBy($model->getTable().'.'.$model->getKeyName());
+		$table = $model->getTable();
+		$keyName = $model->getKeyName();
+		$query = $model->groupBy($table . '.' . $keyName);
+		$dbQuery = $query->getQuery();
+		$countQuery = $db->table($table)->groupBy($table . '.' . $keyName);
 
 		//set up initial array states for the selects
-		$selects = array($model->getTable().'.*');
+		$selects = array($table.'.*');
 
 		//set the filters
-		$this->setFilters($filters, $db_query, $count_query, $selects);
+		$this->setFilters($filters, $dbQuery, $countQuery, $selects);
 
 		//set the selects
-		$db_query->select($selects);
+		$dbQuery->select($selects);
 
 		//determines if the sort should have the table prefixed to it
 		$sortOnTable = true;
@@ -108,7 +110,7 @@ class DataTable {
 			$column->filterQuery($selects);
 
 			//if this is a related field or
-			if ( ($column->isRelated || $column->select) && $column->field === $sort['field'])
+			if ( ($column->getOption('is_related') || $column->getOption('select')) && $column->getOption('column_name') === $sort['field'])
 			{
 				$sortOnTable = false;
 			}
@@ -117,26 +119,15 @@ class DataTable {
 		//if the sort is on the model's table, prefix the table name to it
 		if ($sortOnTable)
 		{
-			$sort['field'] = $model->getTable() . '.' . $sort['field'];
+			$sort['field'] = $table . '.' . $sort['field'];
 		}
 
-		$sql = $query->toSql();
-
-		//then wrap the inner table and perform the count
-		$sql = "SELECT COUNT({$model->getKeyName()}) AS aggregate FROM ({$sql}) AS agg";
-
-		//then perform the count query
-		$results = $count_query->getConnection()->select($sql, $count_query->getBindings());
-		$num_rows = $results[0]->aggregate;
-		$page = (int) $page;
-		$last = (int) ceil($num_rows / $this->rowsPerPage);
-
-		//if the current page is greater than the last page, set the current page to the last page
-		$page = $page > $last ? $last : $page;
+		//run the count query
+		$output = $this->performCountQuery($countQuery, $query->toSql(), $page);
 
 		//now we need to limit and offset the rows in remembrance of our dear lost friend paginate()
 		$query->take($this->rowsPerPage);
-		$query->skip($this->rowsPerPage * ($page === 0 ? $page : $page - 1));
+		$query->skip($this->rowsPerPage * ($output['page'] === 0 ? $output['page'] : $output['page'] - 1));
 
 		//order the set by the model table's id
 		$query->orderBy($sort['field'], $sort['direction']);
@@ -144,20 +135,52 @@ class DataTable {
 		//then retrieve the rows
 		$query->getQuery()->select($selects);
 
+		//parse the results
+		$output['results'] = $this->parseResults($query->distinct()->get());
+
+		return $output;
+	}
+
+	/**
+	 * Performs the count query and returns info about the pages
+	 *
+	 * @param Illuminate\Database\Query\Builder		$countQuery
+	 * @param string								$querySql
+	 * @param int									$page
+	 *
+	 * @return array
+	 */
+	public function performCountQuery(\Illuminate\Database\Query\Builder $countQuery, $querySql, $page)
+	{
+		//grab the model instance
+		$model = $this->config->getDataModel();
+
+		//then wrap the inner table and perform the count
+		$sql = "SELECT COUNT({$model->getKeyName()}) AS aggregate FROM ({$querySql}) AS agg";
+
+		//then perform the count query
+		$results = $countQuery->getConnection()->select($sql, $countQuery->getBindings());
+		$numRows = $results[0]->aggregate;
+		$page = (int) $page;
+		$last = (int) ceil($numRows / $this->rowsPerPage);
+
 		return array(
-			'page' => $page,
+			//if the current page is greater than the last page, set the current page to the last page
+			'page' => $page > $last ? $last : $page,
 			'last' => $last,
-			'total' => $num_rows,
-			'results' => $this->parseResults($query->distinct()->get()),
+			'total' => $numRows,
 		);
 	}
 
 	/**
 	 * Sets the query filters when getting the rows
 	 *
-	 * @param array		$filters
+	 * @param mixed									$filters
+	 * @param Illuminate\Database\Query\Builder		$query
+	 * @param Illuminate\Database\Query\Builder		$countQuery
+	 * @param array									$selects
 	 */
-	public function setFilters($filters, &$db_query, &$count_query, &$selects)
+	public function setFilters($filters, &$query, &$countQuery, &$selects)
 	{
 		//then we set the filters
 		if ($filters && is_array($filters))
@@ -171,8 +194,8 @@ class DataTable {
 				$fieldObject->setFilter($filter);
 
 				//filter the query objects, only pass in the selects the first time so they aren't added twice
-				$fieldObject->filterQuery($db_query, $selects);
-				$fieldObject->filterQuery($count_query);
+				$fieldObject->filterQuery($query, $selects);
+				$fieldObject->filterQuery($countQuery);
 			}
 		}
 	}
@@ -187,11 +210,6 @@ class DataTable {
 	public function parseResults($rows)
 	{
 		$results = array();
-		$columns = $this->columnFactory->getColumns();
-		$includedColumns = $this->columnFactory->getIncludedColumns($this->fieldFactory->getEditFields());
-		$relatedColumns = $this->columnFactory->getRelatedColumns();
-		$onTableColumns = array_merge($includedColumns, $relatedColumns);
-		$computedColumns = $this->columnFactory->getComputedColumns();
 
 		//convert the resulting set into arrays
 		foreach ($rows as $item)
@@ -199,39 +217,76 @@ class DataTable {
 			//iterate over the included and related columns
 			$arr = array();
 
-			foreach ($onTableColumns as $field => $col)
-			{
-				//if this column is in our objects array, render the output with the given value
-				if (isset($columns[$field]))
-				{
-					$arr[$field] = array(
-						'raw' => $item->getAttribute($field),
-						'rendered' => $columns[$field]->renderOutput($item->getAttribute($field)),
-					);
-				}
-				//otherwise it's likely the primary key column which wasn't included (though it's needed for identification purposes)
-				else
-				{
-					$arr[$field] = array(
-						'raw' => $item->getAttribute($field),
-						'rendered' => $item->getAttribute($field),
-					);
-				}
-			}
+			$this->parseOnTableColumns($item, $arr);
 
 			//then grab the computed, unsortable columns
-			foreach ($computedColumns as $col)
-			{
-				$arr[$col] = array(
-					'raw' => $item->{$col},
-					'rendered' => $columns[$col]->renderOutput($item->{$col}),
-				);
-			}
+			$this->parseComputedColumns($item, $arr);
 
 			$results[] = $arr;
 		}
 
 		return $results;
+	}
+
+	/**
+	 * Goes through all related columns and sets the proper values for this row
+	 *
+	 * @param Illuminate\Database\Eloquent\Model	$item
+	 * @param array									$outputRow
+	 *
+	 * @return void
+	 */
+	public function parseOnTableColumns($item, array &$outputRow)
+	{
+		$columns = $this->columnFactory->getColumns();
+		$includedColumns = $this->columnFactory->getIncludedColumns($this->fieldFactory->getEditFields());
+		$relatedColumns = $this->columnFactory->getRelatedColumns();
+
+		//loop over both the included and related columns
+		foreach (array_merge($includedColumns, $relatedColumns) as $field => $col)
+		{
+			$attributeValue = $item->getAttribute($field);
+
+			//if this column is in our objects array, render the output with the given value
+			if (isset($columns[$field]))
+			{
+				$outputRow[$field] = array(
+					'raw' => $attributeValue,
+					'rendered' => $columns[$field]->renderOutput($attributeValue),
+				);
+			}
+			//otherwise it's likely the primary key column which wasn't included (though it's needed for identification purposes)
+			else
+			{
+				$outputRow[$field] = array(
+					'raw' => $attributeValue,
+					'rendered' => $attributeValue,
+				);
+			}
+		}
+	}
+
+	/**
+	 * Goes through all computed columns and sets the proper values for this row
+	 *
+	 * @param Illuminate\Database\Eloquent\Model	$item
+	 * @param array									$outputRow
+	 *
+	 * @return void
+	 */
+	public function parseComputedColumns($item, array &$outputRow)
+	{
+		$columns = $this->columnFactory->getColumns();
+		$computedColumns = $this->columnFactory->getComputedColumns();
+
+		//loop over the computed columns
+		foreach ($computedColumns as $name => $column)
+		{
+			$outputRow[$name] = array(
+				'raw' => $item->{$name},
+				'rendered' => $columns[$name]->renderOutput($item->{$name}),
+			);
+		}
 	}
 
 	/**
@@ -275,23 +330,17 @@ class DataTable {
 	 */
 	public function setRowsPerPage(\Illuminate\Session\Store $session, $globalPerPage, $override = null)
 	{
-		if (is_int($override))
+		if ($override)
 		{
-			$session->put('administrator_' . $this->config->getOption('name') . '_rows_per_page', $override);
+			$perPage = (int) $override;
+			$session->put('administrator_' . $this->config->getOption('name') . '_rows_per_page', $perPage);
 		}
 
 		$perPage = $session->get('administrator_' . $this->config->getOption('name') . '_rows_per_page');
 
 		if (!$perPage)
 		{
-			if ($globalPerPage && is_int($globalPerPage))
-			{
-				$perPage = $globalPerPage;
-			}
-			else
-			{
-				$perPage = $this->rowsPerPage;
-			}
+			$perPage = (int) $globalPerPage;
 		}
 
 		$this->rowsPerPage = $perPage;
