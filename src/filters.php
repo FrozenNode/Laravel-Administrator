29edfdae0f9006d1d6105917c78066ad1e5a8808
Jan Hartigan <janhartigan13@gmail.com>
2013-07-28 19:27:57 -0700

    Fixing locale switch submenu title bug (Closes #264)

--- src/filters.php
+++ src/filters.php
@@ -7,14 +7,6 @@ Route::filter('validate_admin', function ()
 {
 	$configFactory = App::make('admin_config_factory');
 
-	//set the locale
-	$locale = Session::get('administrator_locale');
-
-	if ($locale && in_array($locale, Config::get('administrator::administrator.locales')))
-	{
-		App::setLocale($locale);
-	}
-
 	//get the admin check closure that should be supplied in the config
 	$permission = Config::get('administrator::administrator.permission');
 	$response = $permission();
