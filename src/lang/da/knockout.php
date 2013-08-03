8528952a803e3073bfdfa7f3b858009578fff00d
Jonas Spott <jonasspott@gmail.com>
2013-08-03 03:00:42 +0200

    Added 'da - Danish'

---
+++ src/lang/da/knockout.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines for knockout
+	|--------------------------------------------------------------------------
+	|
+	| da - Danish
+	|
+	*/
+
+	'delete_active_item' => 'Er du sikker på at du vil slette? Dit valg kan ikke trækkes tilbage.',
+	'saving' => 'Gemmer...',
+	'saved' => 'Gemmer række.',
+	'deleting' => 'Sletter...',
+	'deleted' => 'Række blev slettet.',
+	'character_left' => ' tegn tilbage',
+	'characters_left' => ' tegn left',
+	'no_results' => 'Ingen resultater fundet',
+	'select_options' => 'Vælg nogle muligheder',
+
+);
