b9af2b55811f88aea43cc0266c33db79b5b3efd5
Simone Pescina <s.pescina@gmail.com>
2013-08-05 17:06:26 +0200

    Create administrator.php

---
+++ src/lang/it/administrator.php
@@ -0,0 +1,55 @@
+<?php
+
+return array(
+
+  /*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| it - Italian
+	|
+	*/
+
+	'dashboard' => 'Dashboard',
+	'edit' => 'Modifica',
+	'filters' => 'Filtri',
+	'loading' => 'Caricamento...',
+	'createnew' => 'Crea Nuovo',
+	'new' => 'Nuovo',
+	'viewitem' => 'Vedi :single',
+	'id' => 'ID',
+	'uploadimage' => 'Carica Immagine',
+	'imageuploading' => 'Caricamento Immagine',
+	'uploadfile' => 'Carica File',
+	'fileuploading' => 'Caricamento File',
+	'none' => 'Nessuno',
+	'all' => 'Tutti',
+	'itemsperpage' => 'Elementi per pagina',
+	'noresults' => 'Nessun risultato',
+	'backtosite' => 'Torna al sito',
+	'logout' => 'Logout',
+
+	'previous' => 'prec.',
+	'next' => 'succ.',
+
+	'close' => 'Chiudi',
+	'delete' => 'Elimina',
+	'save' => 'Salva',
+	'create' => 'Crea',
+	'cancel' => 'Annulla',
+
+	'active' => 'Solo un momento...',
+	'success' => 'Fatto!',
+	'error' => 'Si è verificato un errore durante l\'esecuzione di questa operazione',
+
+	'valid_columns' => "You must provide a valid 'columns' array in each model's config",
+	'valid_title' => "You must provide a valid title and single name in each model's config",
+	'valid_model' => "You must provide a 'model' option in each model's config",
+	'valid_edit' => "You must provide a valid 'edit_fields' array in each model's config",
+	'valid_menu' => "You must provide a valid 'menu' option in the administrator.php config",
+	'valid_config_path' => "You must provide a valid 'model_config_path' in the administrator.php config. The directory must also exist and be readable.",
+	'not_eloquent' => " is not an Eloquent model",
+	'storage_path_permissions' => "You must make your storage path writable in order to make a settings page",
+	'valid_home_page' => "You must provide a valid menu item in the home_page option of your config",
+);
