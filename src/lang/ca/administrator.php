4dad7f57bbc94811595d7283e46f96b959674910
Jaume Sala <jaumesala@gmail.com>
2013-09-17 20:28:45 +0200

    Add Catalan translations.

---
+++ src/lang/ca/administrator.php
@@ -0,0 +1,55 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| ca - Catalan
+	|
+	*/
+
+	'dashboard' => 'Panell general',
+	'edit' => 'Editar',
+	'filters' => 'Filtres',
+	'loading' => 'Carregant...',
+	'createnew' => 'Crear Nou',
+	'new' => 'Nou',
+	'viewitem' => 'Veure :single',
+	'id' => 'ID',
+	'uploadimage' => 'Pujar Imatge',
+	'imageuploading' => 'Pujant Imatge',
+	'uploadfile' => 'Pujar Arxiu',
+	'fileuploading' => 'Pujant Arxiu',
+	'none' => 'Cap',
+	'all' => 'Tots',
+	'itemsperpage' => 'elements per pàgina',
+	'noresults' => 'Sense Resultats',
+	'backtosite' => 'Tornar al lloc Web',
+	'logout' => 'Sortir',
+
+	'previous' => 'anterior',
+	'next' => 'següent',
+
+	'close' => 'Tancar',
+	'delete' => 'Eliminar',
+	'save' => 'Guardar',
+	'create' => 'Crear',
+	'cancel' => 'cancel·lar',
+
+	'active' => 'Un moment...',
+	'success' => '¡Correcte!',
+	'error' => 'S\'ha produït un error en realitzar aquesta acció',
+
+	'valid_columns' => "Ha de proporcionar un array de 'columns' vàlid en la configuració de cada model",
+	'valid_title' => "Ha de proporcionar un títol vàlid i un únic nom en la configuració de cada model",
+	'valid_model' => "Ha de proporcionar una opció 'model' en la configuració de cada model",
+	'valid_edit' => "Ha de proporcionar una matriu (array) 'edit_fields' vàlid en la configuració de cada model",
+	'valid_menu' => "Ha de proporcionar una opció 'menu' vàlida en la configuració administrator.php",
+	'valid_config_path' => "Ha de proporcionar un 'model_config_path' vàlid en la configuració administrator.php. Aquest directori a més ha d'existir i tenir permisos de lectura",
+	'not_eloquent' => " no és un model Eloquent",
+	'storage_path_permissions' => "Ha de permetre escriure a la ruta d'emmagatzematge (storage path) per tal de fer una pàgina de configuració",
+	'valid_home_page' => "Ha de proporcionar un element de menú vàlid en l'opció home_page de la seva configuració",
+);
