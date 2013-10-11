83016ae687220e28c4514c8fbd204db0f5485ad3
Evgeny Golubev <evgeny@golubev.eu>
2013-10-11 13:18:56 +0300

    Fix non editable field syncing.

--- tests/Fields/Relationships/BelongsToManyTest.php
+++ tests/Fields/Relationships/BelongsToManyTest.php
@@ -84,10 +84,18 @@ class BelongsToManyTest extends \PHPUnit_Framework_TestCase {
 		$this->field->build();
 	}
 
+	public function testFillModelNonEditable()
+	{
+		$model = new BelongsToManyEloquentStub;
+		$this->field->shouldReceive('getOption')->times(3)->andReturn('false', 'fieldSort');
+		$this->field->fillModel($model, '3,4,5');
+		$this->assertTrue(!isset($model->rel));
+	}
+
 	public function testFillModelWithSortField()
 	{
 		$model = new BelongsToManyEloquentStub;
-		$this->field->shouldReceive('getOption')->twice()->andReturn('fieldSort', 'sort');
+		$this->field->shouldReceive('getOption')->times(3)->andReturn('true', 'fieldSort', 'sort');
 		$this->field->fillModel($model, '3,4,5');
 		$this->assertTrue(!isset($model->rel));
 	}
@@ -95,7 +103,7 @@ class BelongsToManyTest extends \PHPUnit_Framework_TestCase {
 	public function testFillModelWithoutSortField()
 	{
 		$model = new BelongsToManyEloquentStub;
-		$this->field->shouldReceive('getOption')->twice()->andReturn('fieldNoSort', false);
+		$this->field->shouldReceive('getOption')->times(3)->andReturn('true', 'fieldNoSort', false);
 		$this->field->fillModel($model, '3,4,5');
 		$this->assertTrue(!isset($model->rel));
 	}
