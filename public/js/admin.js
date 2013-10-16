441514b15c87e270e9359b9e7ce1a13b069902cc
Morgon <jmy@morgontech.com>
2013-10-16 02:23:46 -0400

    Allow redirects in custom actions

--- public/js/admin.js
+++ public/js/admin.js
@@ -587,6 +587,11 @@
 								self.globalStatusMessage(messages.success).globalStatusMessageType('success');
 							}
 
+                                                        // if this is a redirect, do so
+                                                        if (response.redirect) {
+                                                        	window.location.href = response.redirect;
+                                                        }
+                                                        
 							//if there was a file download initiated, redirect the user to the file download address
 							if (response.download)
 								self.downloadFile(response.download);
