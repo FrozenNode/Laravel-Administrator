57ca72c1a8aa977053095d7eda61fab1b8b3caf7
Jan Hartigan <janhartigan13@gmail.com>
2013-12-21 14:15:19 -0800

    adding laravel 4.1 compatibility (See #414, #419)

--- src/controllers/AdminController.php
+++ src/controllers/AdminController.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Routing\Controllers\Controller;
+use AdministratorBaseController as Controller;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Request;
