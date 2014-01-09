1ac6cc433ebdfc42372c11bde0f66115eb9d0c72
wic <wic@qq.com>
2014-01-09 09:35:53 +0800

    add lang zh-TW

--- src/lang/zh-TW/knockout.php
+++ src/lang/zh-TW/knockout.php
@@ -7,18 +7,18 @@ return array(
 	| Administrator Language Lines for knockout
 	|--------------------------------------------------------------------------
 	|
-	| zh-CN - 简体中文
+	| zh-TW - 繁體中文
 	|
 	*/
 
-	'delete_active_item' => '你确定要删除这个条目？这个操作无法逆转。',
+	'delete_active_item' => '你確定要刪除這個條目？這個操作無法逆轉。',
 	'saving' => '保存中...',
-	'saved' => '条目已经保存',
-	'deleting' => '删除中...',
-	'deleted' => '条目已被删除',
+	'saved' => '條目已經保存',
+	'deleting' => '刪除中...',
+	'deleted' => '條目已被刪除',
 	'character_left' => ' 剩余字符',
 	'characters_left' => ' 剩余字符串',
-	'no_results' => '没有结果匹配',
-	'select_options' => '选择一些选项',
+	'no_results' => '沒有結果匹配',
+	'select_options' => '選擇一些選項',
 
 );
