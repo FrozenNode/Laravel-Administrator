1b315a522cf525575687cb510b2458a21b83a013
unknown <dcrystalj@gmail.com>
2014-01-11 00:07:35 +0100

    Slovenian language

---
+++ src/lang/si/knockout.php
@@ -0,0 +1,24 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines for knockout
+	|--------------------------------------------------------------------------
+	|
+	| si - Slovenian
+	|
+	*/
+
+	'delete_active_item' => 'Ali ste prepričani da želite izbrisati ta element? Te operacije ni mogoče razveljaviti',
+	'saving' => 'Shranjevanje...',
+	'saved' => 'Shranjeno.',
+	'deleting' => 'Brisanje...',
+	'deleted' => 'Izbrisano.',
+	'character_left' => ' znak vam je še na voljo',
+	'characters_left' => ' znakov vam je še na voljo',
+	'no_results' => 'Ni rezultatov ujemanja',
+	'select_options' => 'Izbira',
+
+);
