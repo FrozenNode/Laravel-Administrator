69190e037b8f250bacfd6576bc6c87afd41f34bd
Nille af Ekenstam <n@nille.org>
2014-01-21 09:30:15 +0100

    Added Swedish (se) translation.

---
+++ src/lang/se/administrator.php
@@ -0,0 +1,55 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| se - Swedish
+	|
+	*/
+
+	'dashboard' => 'Kontrollpanel',
+	'edit' => 'Redigera',
+	'filters' => 'Filter',
+	'loading' => 'Laddar...',
+	'createnew' => 'Skapa ny',
+	'new' => 'Ny',
+	'viewitem' => 'Visa :enskild',
+	'id' => 'ID',
+	'uploadimage' => 'Ladda upp bild',
+	'imageuploading' => 'Bild laddas upp',
+	'uploadfile' => 'Ladda upp fil',
+	'fileuploading' => 'Fil laddas upp',
+	'none' => 'Ingen',
+	'all' => 'Alla',
+	'itemsperpage' => 'objekt per sida',
+	'noresults' => 'Inget resultat',
+	'backtosite' => 'Tillbaka till sajten',
+	'logout' => 'Logga ut',
+
+	'previous' => 'föregående',
+	'next' => 'nästa',
+
+	'close' => 'Stäng',
+	'delete' => 'Radera',
+	'save' => 'Spara',
+	'create' => 'Skapa',
+	'cancel' => 'Avbryt',
+
+	'active' => 'Ett ögonblick...',
+	'success' => 'Klart!',
+	'error' => 'Ett fel uppstod i samband med denna operation',
+
+	'valid_columns' => "Du måste ange en giltig 'columns'-array i varje modells konfiguration",
+	'valid_title' => "Du måste ange en giltig 'title' och 'single name' i varje modells konfiguration",
+	'valid_model' => "Du måste ange en 'model'-option i varje modells konfiguration",
+	'valid_edit' => "Du måste ange en giltig 'edit_fields'-array i varje modells konfiguration",
+	'valid_menu' => "Du måste ange en giltig 'menu'-option i administrator.php:s konfiguration",
+	'valid_config_path' => "Du måste ange en giltig 'model_config_path' i administrator.php:s konfiguration. Katalogen måste även existera samt vara skrivbar.",
+	'not_eloquent' => " är inte en Eloquent-modell",
+	'storage_path_permissions' => "Du måste göra din 'storage path' skrivbar för att skapa en inställningssida (settings page)",
+	'valid_home_page' => "Du måste ange en giltig 'menu item' i 'home_page'-inställningen i din konfiguration",
+);
