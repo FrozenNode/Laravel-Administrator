eb85c1636647eaeb9213d228468fe6f7b067c284
Jan Hartigan <janhartigan13@gmail.com>
2014-03-09 20:07:16 -0700

    converting action to optionable trait (and php 5.4)

--- src/Frozennode/Administrator/Actions/Action.php
+++ src/Frozennode/Administrator/Actions/Action.php
@@ -1,17 +1,12 @@
 <?php
 namespace Frozennode\Administrator\Actions;
 
-use Frozennode\Administrator\Validator;
 use Frozennode\Administrator\Config\ConfigInterface;
+use Frozennode\Administrator\Traits\OptionableTrait;
 
 class Action {
 
-	/**
-	 * The validator instance
-	 *
-	 * @var \Frozennode\Administrator\Validator
-	 */
-	protected $validator;
+	use OptionableTrait;
 
 	/**
 	 * The config instance
@@ -21,25 +16,11 @@ class Action {
 	protected $config;
 
 	/**
-	 * The user supplied options array
-	 *
-	 * @var array
-	 */
-	protected $suppliedOptions = array();
-
-	/**
-	 * The options array
-	 *
-	 * @var array
-	 */
-	protected $options = array();
-
-	/**
 	 * The default configuration options
 	 *
 	 * @var array
 	 */
-	protected $defaults = array(
+	protected $defaultOptions = array(
 		'title' => 'Custom Action',
 		'has_permission' => true,
 		'confirmation' => false,
@@ -65,33 +46,13 @@ class Action {
 	/**
 	 * Create a new action Factory instance
 	 *
-	 * @param \Frozennode\Administrator\Validator 				$validator
 	 * @param \Frozennode\Administrator\Config\ConfigInterface	$config
 	 * @param array												$options
 	 */
-	public function __construct(Validator $validator, ConfigInterface $config, array $options)
+	public function __construct(ConfigInterface $config, array $options)
 	{
 		$this->config = $config;
-		$this->validator = $validator;
-		$this->suppliedOptions = $options;
-	}
-
-	/**
-	 * Validates the supplied options
-	 *
-	 * @return void
-	 */
-	public function validateOptions()
-	{
-		//override the config
-		$this->validator->override($this->suppliedOptions, $this->rules);
-
-		//if the validator failed, throw an exception
-		if ($this->validator->fails())
-		{
-			throw new \InvalidArgumentException("There are problems with your '" . $this->suppliedOptions['action_name'] . "' action in the " .
-									$this->config->getOption('name') . " model: " .	implode('. ', $this->validator->messages()->all()));
-		}
+		$this->options = $options;
 	}
 
 	/**
@@ -99,7 +60,7 @@ class Action {
 	 *
 	 * @return void
 	 */
-	public function build()
+	public function buildOptions()
 	{
 		$options = $this->suppliedOptions;
 
@@ -107,7 +68,7 @@ class Action {
 		$this->buildStringOrCallable($options, array('confirmation', 'title'));
 
 		//build the string or callable values for the messages
-		$messages = $this->validator->arrayGet($options, 'messages', array());
+		$messages = array_get($options, 'messages', []);
 		$this->buildStringOrCallable($messages, array('active', 'success', 'error'));
 		$options['messages'] = $messages;
 
@@ -131,7 +92,7 @@ class Action {
 		foreach ($keys as $key)
 		{
 			//check if the key's value was supplied
-			$suppliedValue = $this->validator->arrayGet($options, $key);
+			$suppliedValue = array_get($options, $key);
 
 			//if it's a string, simply set it
 			if (is_string($suppliedValue))
@@ -159,46 +120,4 @@ class Action {
 		return $action($data);
 	}
 
-	/**
-	 * Gets all user options
-	 *
-	 * @param bool	$override
-	 *
-	 * @return array
-	 */
-	public function getOptions($override = false)
-	{
-		//if override is true, unset the current options
-		$this->options = $override ? array() : $this->options;
-
-		//make sure the supplied options have been merged with the defaults
-		if (empty($this->options))
-		{
-			//validate the options and build them
-			$this->validateOptions();
-			$this->build();
-			$this->options = array_merge($this->defaults, $this->suppliedOptions);
-		}
-
-		return $this->options;
-	}
-
-	/**
-	 * Gets a field's option
-	 *
-	 * @param string 	$key
-	 *
-	 * @return mixed
-	 */
-	public function getOption($key)
-	{
-		$options = $this->getOptions();
-
-		if (!array_key_exists($key, $options))
-		{
-			throw new \InvalidArgumentException("An invalid option was searched for in the '" . $options['action_name'] . "' action");
-		}
-
-		return $options[$key];
-	}
 }
