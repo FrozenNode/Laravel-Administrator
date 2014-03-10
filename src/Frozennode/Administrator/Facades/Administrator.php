fa232c8bd5b3919ec6261a4d1d8470aac46f55d9
Jan Hartigan <janhartigan13@gmail.com>
2014-03-09 20:05:07 -0700

    creating the facade which will sit in front of the admin manager

---
+++ src/Frozennode/Administrator/Facades/Administrator.php
@@ -0,0 +1,14 @@
+<?php namespace Frozennode\Administrator\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Administrator extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'admin.manager'; }
+
+}
