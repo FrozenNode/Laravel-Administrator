ee7b321e4ce435c4f1fe4bc4a375412ff7fdb0f3
janhartigan <janhartigan13@gmail.com>
2014-10-29 16:17:11 -0700

    adding norwegian bokmal

---
+++ src/lang/nb/administrator.php
@@ -0,0 +1,55 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| nb - Norweigan (Bokmål)
+	|
+	*/
+
+	'dashboard' => 'Kontrollpanel',
+	'edit' => 'Rediger',
+	'filters' => 'Filtrer på',
+	'loading' => 'Laster...',
+	'createnew' => 'Opprett ny',
+	'new' => 'Ny',
+	'viewitem' => 'Vis :single',
+	'id' => 'ID',
+	'uploadimage' => 'Last opp bilde',
+	'imageuploading' => 'Bilde lastes opp',
+	'uploadfile' => 'Last opp fil',
+	'fileuploading' => 'Fil lastes opp',
+	'none' => 'Ingen',
+	'all' => 'Alle',
+	'itemsperpage' => 'rekker pr. side',
+	'noresults' => 'Ingen resultater',
+	'backtosite' => 'Tilbake til siden',
+	'logout' => 'Logg ut',
+
+	'previous' => 'forrige',
+	'next' => 'neste',
+
+	'close' => 'Lukk',
+	'delete' => 'Slett',
+	'save' => 'Lagre',
+	'create' => 'Opprett',
+	'cancel' => 'Avbryt',
+
+	'active' => 'Et øyeblikk...',
+	'success' => 'Suksess!',
+	'error' => 'En feil oppstod under handlingen',
+
+	'valid_columns' => "Du må oppgi en gyldig 'columns'-array i hver modell-konfigurasjon",
+	'valid_title' => "Du må oppgi en gyldig tittel og et gyldig tittelnavn i hver modell-konfigurasjon",
+	'valid_model' => "Du må oppgi en 'model'-option i hver modell-konfigurasjon",
+	'valid_edit' => "Du må oppgi en gyldig 'edit_fields'-array i hver modell-konfigurasjon",
+	'valid_menu' => "Du må oppgi en gyldig 'menu'-option i administrator.php:s konfigurasjonen",
+	'valid_config_path' => "Du må oppgi en gyldig 'model_config_path' i administrator.php:s konfigurasjonen. Mappen må også eksistere og må være leselig (readable).",
+	'not_eloquent' => " er ikke en Eloquent-modell",
+	'storage_path_permissions' => "Du må gjøre din 'storage path' skrivelig(writable) for å opprette en innstillingsside (settings page)",
+	'valid_home_page' => "Du må oppgi en gyldig 'menu item' i 'home_page'-innstillingen i din konfigurasjon",
+);
---
+++ src/lang/nb/knockout.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines for knockout
+	|--------------------------------------------------------------------------
+	|
+	| nb - Norweigan (Bokmål)
+	|
+	*/
+
+	'delete_active_item' => 'Er du sikker på at du vil slette? Ditt valg kan ikke trekkes tilbake.',
+	'saving' => 'Lagrer...',
+	'saved' => 'Lagret.',
+	'deleting' => 'Sletter...',
+	'deleted' => 'Slettet.',
+	'character_left' => ' tegn igjen',
+	'characters_left' => ' tegn igjen',
+	'no_results' => 'Ingen resultater funnet',
+	'select_options' => 'Velg noen muligheter',
+
+);
