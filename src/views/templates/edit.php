670cf370891baea3a7276e06e949616a6aa9456f
Zsombor Franczia <frzsombor@freemail.hu>
2014-11-03 18:59:46 +0100

    Hide "characters left" if editing is disabled on TEXTAREA fields

--- src/views/templates/edit.php
+++ src/views/templates/edit.php
@@ -36,9 +36,9 @@
 			<!-- /ko -->
 
 			<!-- ko if: type === 'textarea' -->
-				<div class="characters_left" data-bind="charactersLeft: {value: $root[field_name], limit: limit}"></div>
 				<!-- ko if: editable -->
-				<textarea data-bind="attr: {disabled: $root.freezeForm || !editable, id: field_id}, value: $root[field_name],
+					<div class="characters_left" data-bind="charactersLeft: {value: $root[field_name], limit: limit}"></div>
+					<textarea data-bind="attr: {disabled: $root.freezeForm || !editable, id: field_id}, value: $root[field_name],
 																		valueUpdate: 'afterkeydown', characterLimit: limit,
 																		style: {height: height + 'px'}"></textarea>
 				<!-- /ko -->
