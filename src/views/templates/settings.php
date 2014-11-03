b415cdbe1d22a3519f581bcb07be07b98a992864
Zsombor Franczia <frzsombor@freemail.hu>
2014-11-03 19:38:28 +0100

    Added "editable" check and "Label (value)" style appearance for ENUM field

--- src/views/templates/settings.php
+++ src/views/templates/settings.php
@@ -78,8 +78,13 @@
 			<!-- /ko -->
 
 			<!-- ko if: type === 'enum' -->
-				<input type="hidden" data-bind="attr: {disabled: $root.freezeForm, id: field_id}, value: $root[field_name],
-												select2: {data: {results: options}}" />
+				<!-- ko if: editable -->
+					<input type="hidden" data-bind="attr: {disabled: $root.freezeForm, id: field_id}, value: $root[field_name],
+													select2: {data: {results: options}}" />
+				<!-- /ko -->
+				<!-- ko ifnot: editable -->
+					<div class="uneditable" data-bind="enumText: { value: $root[field_name](), enumOptions: options }"></div>
+				<!-- /ko -->
 			<!-- /ko -->
 
 			<!-- ko if: type === 'date' -->
