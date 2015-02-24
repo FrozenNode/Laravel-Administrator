b0767dcb9aad107aacc1850f9ccee24567932077
Nurlan Alekberov <nurlan.alekberoff@gmail.com>
2015-02-24 14:23:07 +0400

    Azeri localization has been added.

---
+++ src/lang/az/knockout.php
@@ -0,0 +1,24 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines for knockout
+	|--------------------------------------------------------------------------
+	|
+	| az - Azeri
+	|
+	*/
+
+	'delete_active_item' => 'Bunu silməyə əminsiniz? Bu geri qaytarıla bilməyəcək.',
+	'saving' => 'Saxlanır...',
+	'saved' => 'Saxlandı.',
+	'deleting' => 'Silinir...',
+	'deleted' => 'Silindi.',
+	'character_left' => ' xarakter qaldı',
+	'characters_left' => ' xarakter qaldı',
+	'no_results' => 'Uyğun nəticə gəlmədi',
+	'select_options' => 'Bəzi seçənəklər seçin',
+
+);
