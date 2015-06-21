b34932d927f43932b49bae3ba8ca0408d1ef28ec
sorinfederiga <gioviboy99@yahoo.com>
2015-06-21 23:14:05 +0300

    added romanian translation

--- src/lang/ro/knockout.php
+++ src/lang/ro/knockout.php
@@ -7,7 +7,7 @@ return array(
 	| Administrator Language Lines for knockout
 	|--------------------------------------------------------------------------
 	|
-	| en - English
+	| ro - Romanian
 	|
 	*/
 
