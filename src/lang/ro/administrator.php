b09775a9c81b87cef543c23cbd3f84355f26268d
sorinfederiga <gioviboy99@yahoo.com>
2015-06-21 23:11:09 +0300

    added romanian translation

---
+++ src/lang/ro/administrator.php
@@ -0,0 +1,57 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Administrator Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| en - English
+	|
+	*/
+
+	'dashboard' => 'Dashboard',
+	'edit' => 'Edit',
+	'filters' => 'Filtre',
+	'loading' => 'Încarcă...',
+	'createnew' => 'Crează ',
+	'new' => 'Crează',
+	'viewitem' => 'Vizualizează :single',
+	'id' => 'ID',
+	'uploadimage' => 'Încarcă imagine',
+	'imageuploading' => 'Încarc imaginea',
+	'uploadfile' => 'Încarcă fișier',
+	'fileuploading' => 'Încarc fișier',
+	'no_image_uploaded' => 'Nicio imagine încărcată',
+	'no_file_uploaded' => 'Niciun fișier încărcat',
+	'none' => 'Nimic',
+	'all' => 'Toate',
+	'itemsperpage' => 'inregistrări pe pagină',
+	'noresults' => 'Niciun rezultat',
+	'backtosite' => 'Inapoi la site',
+	'logout' => 'Logout',
+
+	'previous' => 'inapoi',
+	'next' => 'inainte',
+
+	'close' => 'Închide',
+	'delete' => 'Șterge',
+	'save' => 'Salvează',
+	'create' => 'Crează',
+	'cancel' => 'Abandon',
+
+	'active' => 'Un moment...',
+	'success' => 'Succes!',
+	'error' => 'Eroare la executarea acțiunii',
+
+    'valid_columns' => "Specifică un array valid 'columns' pentru configurarea modelului ",
+    'valid_title' => "Specifică un titlu și un nume valid pentru configurarea modelului ",
+    'valid_model' => "Specifică un 'model' pentru configurarea modelului",
+    'valid_edit' => "Specifică un array valid 'edit_fields' pentru configurarea modelului",
+    'valid_menu' => "Specifică optiuni valide de 'menu' în administrator.php",
+    'valid_config_path' => "Specifică 'model_config_path' în administrator.php. Directorul trebuie să existe și să poată fi accesat",
+    'not_eloquent' => " nu este model Eloquent",
+    'storage_path_permissions' => "Directorul de stocare trebuie să aibă permisiuni de scriere pentru a putea folosi setări",
+    'valid_home_page' => "Specifică o opțiune validă de meniu în configurarea home_page ",
+);
