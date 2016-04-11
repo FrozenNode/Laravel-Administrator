f8a3e775f99e0eb9dc07556ad37e6d20aab886bd
Jay Shaffstall <jshaffstall@gmail.com>
2016-04-11 00:40:54 +0000

    Changed url() to url('/') to account for Laravel 5.2

--- src/views/settings.php
+++ src/views/settings.php
@@ -3,7 +3,7 @@
 </div>
 
 <script type="text/javascript">
-	var site_url = "<?php echo url() ?>",
+	var site_url = "<?php echo url('/') ?>",
 		base_url = "<?php echo $baseUrl ?>/",
 		asset_url = "<?php echo $assetUrl ?>",
 		save_url = "<?php echo route('admin_settings_save', array($config->getOption('name'))) ?>",
